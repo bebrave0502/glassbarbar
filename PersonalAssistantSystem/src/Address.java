653864c708fb576a9709d2bf4895a2d3cb6df01d
hyewon <hyewon@172.30.1.39>
2016-12-21 21:16:00 +0900

    address객체 파

---
+++ PersonalAssistantSystem/src/Address.java
@@ -0,0 +1,39 @@
+import java.io.Serializable;
+
+public class Address implements Serializable {
+	private String name;
+	private String phoneNumber;
+	
+	public Address(String name, String phoneNumber) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@Override
+	public String toString() {
+		String description;
+
+		description = "name: " + name +" / ";
+		description += "phone number: " + phoneNumber + "\n";
+
+		return description;
+	}
+	
+	
+}
